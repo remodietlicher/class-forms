47131b83823d680e147fc834ba823d736cbd54af
Remo Dietlicher <remo.d@gmx.ch>
2022-09-17 21:38:15 +0200

    first chain of decorator to html working

--- src/render/components/FormComponent.tsx
+++ src/render/components/FormComponent.tsx
@@ -5,6 +5,6 @@ interface FormProps {
   meta: FormMetadata;
 }
 
-export const FormComponent: React.FC = ({ meta }: FormProps) => {
+export const FormComponent: React.FC<FormProps> = ({ meta }: FormProps) => {
   return <h1>{meta.target.name}</h1>;
 };
