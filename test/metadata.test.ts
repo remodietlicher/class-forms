9067da8e2c33649460eb98ab281de0c548e4b428
Remo Dietlicher <remo.d@gmx.ch>
2022-09-23 23:26:39 +0200

    rename object -> class

--- test/metadata.test.ts
+++ test/metadata.test.ts
@@ -1,21 +1,21 @@
 import { getMetadataStorage } from "../src/globals";
 import { FieldMetadata } from "../src/metadata/FieldMetadata";
 import { FormMetadata } from "../src/metadata/FormMetadata";
-import { SimpleClass } from "./objects/SimpleClass";
+import { SimpleClass } from "./classes/SimpleClass";
 
-describe("Object decorators", () => {
+describe("Class decorators", () => {
   let formMetadata: FormMetadata | undefined;
   let fieldMetadatas: FieldMetadata[] | undefined;
   beforeEach(() => {
     formMetadata = getMetadataStorage().getFormMetadata(SimpleClass);
     fieldMetadatas = getMetadataStorage().getFieldMetadatas(SimpleClass);
   });
-  it("should map object member variables to string lists", () => {
+  it("should map class member variables to string lists", () => {
     const fieldNames = fieldMetadatas?.map((f) => f.propertyKey);
     expect(fieldNames).toEqual(["member1", "member2"]);
   });
 
-  it("should provide additional information in an options object", () => {
+  it("should provide additional information in an options class", () => {
     const options = fieldMetadatas?.map((f) => f.options?.cssClass);
     expect(options).toEqual([undefined, "button on-hover"]);
   });
